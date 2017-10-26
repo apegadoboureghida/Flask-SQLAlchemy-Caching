f73e2a0d1d1fc7cee7e480cead8f67f778844fcc
apegado <apegadoboureghida@users.noreply.github.com>
2017-10-25 19:07:40 -0500

    literal_binds set to False
    UUID class does not implement process_literal_param

--- flask_sqlalchemy_caching/core.py
+++ flask_sqlalchemy_caching/core.py
@@ -129,7 +129,9 @@ class CachingQuery(BaseQuery):
         particular Query, then combining that with the bound parameter values.
         """
         stmt = self.with_labels().statement
-        key = str(stmt.compile(compile_kwargs={'literal_binds': True}))
+        # key = str(stmt.compile(compile_kwargs={'literal_binds': True}))
+        # UUID class does not implement process_literal_param
+        key = str(stmt.compile(compile_kwargs={'literal_binds': False}))
         return md5(key.encode('utf8')).hexdigest()
 
 
